cb3a9e3c07a926810228ccb30aa3e6701fb69c22
samikreal <samikre@yahoo.com>
2020-06-17 18:03:45 +0200

    Make it work?

--- ddm-lmp/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
+++ ddm-lmp/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
@@ -1,19 +1,11 @@
 package de.hpi.ddm.actors;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
-import akka.NotUsed;
 import akka.actor.AbstractLoggingActor;
 import akka.actor.ActorRef;
 import akka.actor.ActorSelection;
 import akka.actor.Props;
-import akka.serialization.*;
-import akka.stream.ActorMaterializer;
-import akka.stream.Materializer;
-import akka.stream.javadsl.Sink;
-import akka.stream.javadsl.Source;
 import de.hpi.ddm.structures.KryoPoolSingleton;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -30,7 +22,8 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 	////////////////////////
 
 	public static final String DEFAULT_NAME = "largeMessageProxy";
-	
+	private ActorRef MasterField_worker_receiver_url;
+
 	public static Props props() {
 		return Props.create(LargeMessageProxy.class);
 	}
@@ -38,13 +31,24 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 	////////////////////
 	// Actor Messages //
 	////////////////////
+
 	//https://projectlombok.org/features/constructor
 	@Data @NoArgsConstructor @AllArgsConstructor
 	public static class LargeMessage<T> implements Serializable {
 		private static final long serialVersionUID = 2940665245810221108L;
 		private T message;
-		private ActorRef receiver;
+		private ActorRef message_receiver_worker_master;
 	}
+
+	@Data
+	@NoArgsConstructor
+	@AllArgsConstructor
+	private static class MasterInquiry implements Serializable { //Send message asking if worker can receive a message
+		private ActorRef master; //master
+		private ActorRef master_largeMessageProxy_url; //largeMessageProxy from master
+		private ActorRef receiver_worker; //worker
+	}
+
 
 	@Data @NoArgsConstructor @AllArgsConstructor
 	public static class BytesMessage<T> implements Serializable {
@@ -62,14 +66,7 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 		private ActorRef sender;
 	}
 
-	@Data
-	@NoArgsConstructor
-	@AllArgsConstructor
-	private static class RequestMessage implements Serializable {
-		private ActorRef master; //master
-		private ActorRef sender; //largeMessageProxy from master
-		private ActorRef receiver; //worker
-	}
+
 
 	private byte[] messageOutgoing; //Whole message that is going to be sent
 	//private byte[] messageIngoing = new byte[0];
@@ -126,6 +123,7 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 	public Receive createReceive() {
 		return receiveBuilder()
 				.match(LargeMessage.class, this::handle)
+				.match(MasterInquiry.class, this::handle)
 				.match(BytesMessage.class, this::handle)
 				.matchAny(object -> this.log().info("Received unknown message: \"{}\"", object.toString()))
 				.build();
@@ -133,8 +131,8 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 
 	//This handler is from the master/largeMessageProxy!
 	private void handle(LargeMessage<?> message) { // 7. Master sends (sender) a LargeMessage to the master/largeMessageProxy (and get received by this handler). Message contains message_info and the url to the worker (receiver) in the LargeMessage message
-		ActorRef receiver = message.getReceiver(); //thi is the worker url which was sent in the message LargeMessage
-		ActorSelection receiverProxy = this.context().actorSelection(receiver.path().child(DEFAULT_NAME));
+		ActorRef receiver = message.getMessage_receiver_worker_master(); //thi is the worker url which was sent in the message LargeMessage
+		ActorSelection receiverProxy = this.context().actorSelection(receiver.path().child(DEFAULT_NAME)); //Here we get worker url
 
 		//Starting kryo serialization (create Source -> create sink -> materialize)
 		//https://doc.akka.io/docs/akka/2.5.22/stream/stream-quickstart.html
@@ -142,86 +140,44 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 		//https://en.wikibooks.org/wiki/Java_Akka_Streams/Sources
 		this.messageOutgoing = KryoPoolSingleton.get().toBytesWithClass(message.getMessage()); //converting data into bytes and saving to array
 
-		//System.out.println("IMPORTANT: " + this.sender()+this.self()+this.receiver);
-		//receiverProxy.tell(new RequestMessage(this.sender(), this.self(), this.receiver), this.self()); //**!Send from worker to master (this.sender() is the master)
+		//System.out.println("IMPORTANT Large Message Proxy /Master: " + this.sender()+this.self()+this.MasterField_worker_receiver_url);
+		receiverProxy.tell(new MasterInquiry(this.sender(), this.self(), this.MasterField_worker_receiver_url), this.self()); //**!Send from worker to master (this.sender() is the master)
+		//TODO: find out where the documentation says we should use ack and confirmation message for streams****
 
-
+		//TODO: MAYBE we need this????
 		//Materializer from actor context
-		final Materializer materializer = ActorMaterializer.create(this.context());
-
-		Creator2 creator = new Creator2(); //TODO
-		Source<Creator2, NotUsed> source = Source.fromIterator(creator);
-
-		/*
-		Sink sink = Sink.actorRefWithAck(
-				message.getSender(),
-				new StreamInitializedMessage(),
-				Ack.INSTANCE,
-				new StreamCompletedMessage(),
-				err -> new StreamFailureMessage(err)
-		);
-
-		 */
-
-		//source.runWith()
-
-
-		//TODO pass creator to source and run it to generate the numbers from iterator
-
-		/*
-		Iterable<Integer> numbersOneToTen = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-		Source<Integer, NotUsed> source = Source.from(numbersOneToTen);
-		Sink<Integer, CompletionStage<Done>> foreach = Sink.foreach(System.out::println);
-		CompletionStage<Done> futureDone = source.runForeach(System.out::println, materializer);
-		futureDone.toCompletableFuture().join();
-
-		//Graph example
-		Iterable<Integer> numbersOneToTen = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-		Source<Integer, NotUsed> source = Source.from(numbersOneToTen);
-		Sink<Integer, CompletionStage<Done>> foreach = Sink.foreach(System.out::println);
-		RunnableGraph<CompletionStage<Done>> graph = source.toMat(foreach, Keep.right());
-		CompletionStage<Done> futureDone = graph.run(materializer);
-		futureDone.toCompletableFuture().join();
-		 */
-
-
-		/*
-		Sink sink = Sink.actorRefWithAck(
-				message.getSender(),
-				new StreamInitializedMessage(),
-				Ack.INSTANCE,
-				new StreamCompletedMessage(),
-				err -> new StreamFailureMessage(err)
-		);
-		Source.fromIterator(creator).runWith(sink, ActorMaterializer.create(this.context()));
-		 */
-
-		//Create iterator to run and send message
-		//Source
-
-
-		//Like this : KryoPoolSingleton.get().toBytesWithClass(message.getMessage());
+		//final Materializer materializer = ActorMaterializer.create(this.context());
+		//Creator2 creator = new Creator2();
+		//Source<Creator2, NotUsed> source = Source.fromIterator(creator);
 
 
 
 		// This will definitely fail in a distributed setting if the serialized message is large!
 		// Solution options:
 		// 1. Serialize the object and send its bytes batch-wise (make sure to use artery's side channel then).
-		// 2. Serialize the object and send its bytes via Akka streaming.
+		// 2. Serialize the object and send its bytes via Akka streaming. OUR CHOICE
 		// 3. Send the object via Akka's http client-server component.
 		// 4. Other ideas ...
-		receiverProxy.tell(new BytesMessage<>(message.getMessage(), this.sender(), message.getReceiver()), this.self());
 	}
 
+	private void handle(MasterInquiry message) { //8. Master/largeMessageProxy sent a MasterInquiry message asking for (permission?) to send large message
+		//TODO: refactor (not working yet)
+		this.MasterField_worker_receiver_url = message.getReceiver_worker();
+		this.master_largeMessageProxy = message.getMaster();
+		message.getMaster_largeMessageProxy_url().tell(new ConfigurationMessage(this.self()), this.self()); //this.self is the LargeMessageProxy
+	}
+
+
+
 	private void handle(BytesMessage<?> message) {
-		// Reassemble the message content, deserialize it and/or load the content from some local location before forwarding its content.
+	// Reassemble the message content, deserialize it and/or load the content from some local location before forwarding its content.
 
-		//Receive message from master ->   handle(LargeMessage<?> message)
-		//final Source<Byte, NotUsed> messageStream = source.scan(BigInteger.ONE, (acc, next) -> acc.multiply(BigInteger.valueOf(next)));
+	//Receive message from master ->   handle(LargeMessage<?> message)
+	//final Source<Byte, NotUsed> messageStream = source.scan(BigInteger.ONE, (acc, next) -> acc.multiply(BigInteger.valueOf(next)));
 
-		//Deserializelike this:
-		//Object deserObj = kryo.fromBytes(myObj);
+	//Deserializelike this:
+	//Object deserObj = kryo.fromBytes(myObj);
 
-		message.getReceiver().tell(message.getBytes(), message.getSender());
+	message.getReceiver().tell(message.getBytes(), message.getSender());
 	}
 }
